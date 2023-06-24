8976e17f0f35603f3c6a7f383cd219c0cf2c53a6
JoseOrdaz <joseordazsuay@gmail.com>
2023-06-24 13:19:34 +0200

    añadir funcion volver con useNavigate y añadir plantilla formulario

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,10 +1,32 @@
 function Cliente({ cliente }) {
-
-    const { nombre, empresa, email, telefono, id } = cliente
+  const { nombre, empresa, email, telefono, id } = cliente;
   return (
     <>
-      <tr>
-        <td className="p-4">{nombre}</td>
+      <tr className="border-b">
+        <td className="p-4 space-y-2">
+            <p className="text-2xl text-gray-800">{nombre}</p>
+            <p>{empresa}</p>
+
+        </td>
+        <td className="p-4 space-y-2">
+            <p className="text-gray-800"><span className="text-gray-800 uppercase font-bold">Email: </span>{email}</p>
+            <p className="text-gray-800"><span className="text-gray-800 uppercase font-bold">Tel: </span>{telefono}</p>
+        </td>
+        <td className="p-4 flex gap-3">
+            <button
+                type="button"
+                className="text-sky-600 uppercase font-bold text-xs"
+            >
+                Editar
+            </button>
+            <button
+                type="button"
+                className="text-red-600 uppercase font-bold text-xs"
+            >
+                Eliminar
+            </button>
+            
+        </td>
       </tr>
     </>
   );
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,8 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import Formulario from "../components/Formulario";
 
-const NuevoCliente = () => {
+function NuevoCliente() {
+  const navigate = useNavigate();
+
   return (
-    <div>Nuevo Cliente</div>
-  )
+    <>
+      <h1 className="font-black text-4xl text-sky-900">Nuevo Cliente</h1>
+      <p className="mt-3">
+        Completa todos los campos para registra un nuevo cliente
+      </p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-sky-900 text-white px-3 py-1 font-bold uppercase rounded"
+          onClick={() => navigate("/")}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form>
+          <Formulario />
+          <input
+            type="submit"
+            className="mt-5 w-full bg-sky-800 p-3 uppercase font-bold text-white rounded text-lg"
+            value="Registrar cliente"
+          ></input>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default NuevoCliente
+export default NuevoCliente;
