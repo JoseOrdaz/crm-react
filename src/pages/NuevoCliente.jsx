58df4f7dc1c3246292ca94b4449e4fa78c958990
JoseOrdaz <joseordazsuay@gmail.com>
2023-06-26 23:00:30 +0200

    Instalar json-server y configurar variables de entorno para leer dato de la api fake

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -8,11 +8,19 @@ export async function action({request}) {
 
   const datos = Object.fromEntries(formData)
 
+  const email = formData.get('email');
+
 
     const errores = [];
 
    if(Object.values(datos).includes('')){
     errores.push('Todos los campos son oblitorios')
+   }
+
+   let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+   if(!regex.test(email)){
+    errores.push('El email no es correcto')
    }
 
    if(Object.keys(errores).length){
@@ -48,15 +56,17 @@ function NuevoCliente() {
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
 
         {errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error>)}
-        
+
         <Form
           method="post"
+          noValidate
         >
           <Formulario />
           <input
             type="submit"
             className="mt-5 w-full bg-sky-800 p-3 uppercase font-bold text-white rounded text-lg"
             value="Registrar cliente"
+            
            />
         </Form>
       </div>
